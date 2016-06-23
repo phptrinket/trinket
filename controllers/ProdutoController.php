7ad13afad4ebea22c742d61acc05b852d1f7c29c
Selene Stephansson <selene.osten@gmail.com>
2016-06-23 00:46:50 -0300

    cadastrando imagem!

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -8,6 +8,7 @@ use app\models\ProdutoSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use app\models\UploadForm;
 
 /**
  * ProdutoController implements the CRUD actions for Produto model.
