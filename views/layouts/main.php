114478748b427391231bbafe69c18487d0c99775
daizinunes <daizinunes@hotmail.com>
2016-06-23 21:54:13 -0300

    Update para mudar o nav-bar conforme o usuário está logado/deslogado.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -81,6 +81,7 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
+    
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
